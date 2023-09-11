10409497e1b3d0a55d969633fb32d0d8b3edf553
sasi-kt <sasikiran157@gmail.com>
2023-09-12 00:59:39 +0200

    Solve func

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify
+import random
+from itertools import islice
 
 app = Flask(__name__)
 
@@ -22,21 +24,59 @@ sudoku_board = [ [nums[pattern(r,c)] for c in cols] for r in rows ]
 squares = side*side
 empties = squares * 3//4
 for p in sample(range(squares),empties):
-    sudoku_board[p//side][p%side] = None
+    sudoku_board[p//side][p%side] = 0
+
+def Solve(board):
+    side   = len(board)
+    base   = int(side**0.5)
+    board  = [n for row in board for n in row ]
+    blanks = [i for i,n in enumerate(board) if n == 0 ]
+    cover  = { (n,p):{*zip([2*side+r, side+c, r//base*base+c//base],[n]*(n and 3))}
+                for p in range(side*side) for r,c in [divmod(p,side)] for n in range(side+1) }
+    used   = set().union(*(cover[n,p] for p,n in enumerate(board) if n))
+    placed = 0
+    while placed>=0 and placed<len(blanks):
+        pos        = blanks[placed]
+        used      -= cover[board[pos],pos]
+        board[pos] = next((n for n in range(board[pos]+1,side+1) if not cover[n,pos]&used),0)
+        used      |= cover[board[pos],pos]
+        placed    += 1 if board[pos] else -1
+        if placed == len(blanks):
+            solution = [board[r:r+side] for r in range(0,side*side,side)]
+            yield solution
+            placed -= 1
+
+
+solved_board = next(Solve(sudoku_board))
+
+while True:
+    solved  = [*islice(Solve(sudoku_board),2)]
+    if len(solved)==1:break
+    diffPos = [(r,c) for r in range(9) for c in range(9)
+               if solved[0][r][c] != solved[1][r][c] ] 
+    r,c = random.choice(diffPos)
+    sudoku_board[r][c] = solved_board[r][c]
 
 @app.route('/')
 def index():
     return render_template('index.html', board=sudoku_board)
 
-#@app.route('/solve', methods=['POST'])
-#def solve():
+@app.route('/solve', methods=['POST'])
+def check():
     # Get the Sudoku board from the frontend
-    board = request.json['board']
+    user_board = request.json['board']
 
-    # Call the Sudoku solver function (implement this)
-    solved_board = sudoku_solver.solve(board)
+    # Check for errors by comparing the user's board with the solved board
+    errors = []
+    for i in range(9):
+        for j in range(9):
+            if user_board[i][j] != solved_board[i][j]:
+                errors.append({'row': i, 'col': j})
 
-    return jsonify({'solved_board': solved_board})
+    if len(errors) == 0:
+        return jsonify({'message': 'Congratulations! Sudoku is solved correctly.'})
+    else:
+        return jsonify({'errors': errors})
 
 if __name__ == '__main__':
     app.run(debug=True)
